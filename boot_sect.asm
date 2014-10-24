7da87c392477fd270f10313e64629ad21d82791b
Chunyang Xu <xuchunyang56@gmail.com>
2014-10-24 19:50:05 +0800

    Use BIOS interrupt to display char

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,8 +1,12 @@
-; boot_sect.asm -- A simple boot sector program that loops forever.
+; boot_sect.asm -- A simple boot sector program that print message vis BIOS
 
 bits 16
 
 start:
+    mov ah, 0x0e
+    mov al, 'H'
+    int 0x10     ; (INT 0x10 / AH=0x0E) -> Video display char
+
     jmp $
 
 times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
