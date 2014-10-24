33d207bba6785bb15a84799fd947f1a74c24815d
Chunyang Xu <xuchunyang56@gmail.com>
2014-10-24 20:46:34 +0800

    boot: correct label addressing
    boot sector is loaded at 0x7C00 by BIOS, so all label need to added 0x7c00

--- boot_sect.asm
+++ boot_sect.asm
@@ -1,13 +1,17 @@
 ; boot_sect.asm -- A simple boot sector program that print message vis BIOS
 
-bits 16
+bits 16                   
+org 0x7c00 ; tell NASM to correct address of label
 
 start:
     mov ah, 0x0e
-    mov al, 'H'
-    int 0x10     ; (INT 0x10 / AH=0x0E) -> Video display char
+
+    mov al, [sample_char] ; do not need to fix label address now
+    int 0x10
 
     jmp $
+
+sample_char: db 'X' ; find its absoult memory address
 
 times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
 dw 0xAA55               ; boot signature
