ab20a07e6198aaa9984cfc0c56ed456463067f04
Chunyang Xu <xuchunyang56@gmail.com>
2014-10-24 23:59:09 +0800

    boot: Switch to Protected Mode

--- boot_sect.asm
+++ boot_sect.asm
@@ -4,55 +4,26 @@ bits 16
 org 0x7c00                      ; tell NASM to correct address of label
 
 start:
-    mov [BOOT_DRIVE], dl        ; BIOS store our boot driver in DL, save it
-
     mov bp, 0x8000              ; Set stack in a safe place
     mov sp, bp                  ; at 0x8000
 
-    mov bx, 0x9000              ; Load 5 sectors (2-6) to 0x0000(ES):0x9000(BX)
-    mov dh, 5                   ; from the boot disk
-    mov dl, [BOOT_DRIVE]
-    call disk_load
-
     mov si, boot_msg
     call print_str
-    
-    mov dx, [0x9000 + 512]
-    mov [reg16], dx
-    call printreg16
 
-    mov dx, [0x9000 + 512 - 1]
-    mov [reg16], dx
-    call printreg16
+    ; todo load kernel (from disk)
+
+;----------Entering Protected Mode----------;
+enter_pm:
+    cli
+    lgdt [gdt_desc]         ; Load the GDT descriptor
+
+    mov eax, cr0            ; Copy the contents of CR0 into EAX
+    or eax, 1               ; Set bit 0     (0xFE = Real Mode)
+    mov cr0, eax            ; Copy the contents of EAX into CR0
+
+    jmp KERNEL_CODE:init_pm ; Jump to code segment, offset init_pm
 
     jmp $
-
-;---------------------------------------------------------------
-; load DH sectors to ES:BX from drive DL
-disk_load:
-    push dx                     ; Store DX on stack so later we can recall 
-                                ; how many sectors were request to be read
-    mov ah, 0x02                ; BIOS read sector function
-    mov al, dh                  ; Read DH sectors
-    mov ch, 0x00                ; Select cylinder 0 
-    mov dh, 0x00                ; Select head 0
-    mov cl, 0x02                ; Start reading from second sector (i.e.
-                                ; after the boot sector)
-    int 0x13                    ; BIOS interrupt (INT 0x13, AH = 2 -- read floppy/hard disk in CHS mode)
-
-    jc disk_error               ; Jump if error (i.e. carry flag set)
-    
-    pop dx                      ; Restore DX from the stack
-    cmp dh, al                  ; if AL (sectors read) != DH (sectors expected)
-    jne disk_error              ;    display error message
-    ret
-
-disk_error:
-    
-    mov si, DISK_ERROR_MSG
-    call print_str
-    jmp $
-
 
 ;---------------------------------------------------------------
 ; @si (address of string)
@@ -68,42 +39,80 @@ print_str:
     ret
 
 ;---------------------------------------------------------------
-; @ [reg16] (save double-bytes to [reg16]
-printreg16:
-   mov di, outstr16 + 2
-   mov ax, [reg16]
-   mov si, hexstr
-   mov cx, 4            ; four places
-hexloop:
-   rol ax, 4            ; leftmost will
-   mov bx, ax           ; become
-   and bx, 0x0f         ; rightmost
-   mov bl, [si + bx]    ;index into hexstr
-   mov [di], bl
-   inc di
-   dec cx
-   jnz hexloop
- 
-   mov si, outstr16
-   call print_str
- 
-   ret
- 
-
-;---------------------------------------------------------------
 ; Data
-boot_msg: 
-    db 'Booting OS...', 0
-; Variable
-HEX_OUT: db '0x0000', 0
+boot_msg db 'Booting OS...', 0
+HEX_OUT  db '0x0000', 0
 hexstr   db '0123456789ABCDEF'
-outstr16   db '0x0000', 0  ;register value string
-reg16   dw    0         ; pass values to printreg16
+outstr16 db '0x0000', 0  ;register value string
+reg16    dw    0         ; pass values to printreg16
 DISK_ERROR_MSG db "Disk read error!", 0
 BOOT_DRIVE db 0
 
+;----------Global Descriptor Table----------;
+
+gdt:                            ; Address for the GDT
+
+gdt_null:                       ; Null Segment
+        dd 0
+        dd 0
+
+
+KERNEL_CODE             equ $-gdt
+gdt_kernel_code:
+        dw 0FFFFh               ; Limit 0xFFFF
+        dw 0                    ; Base 0:15
+        db 0                    ; Base 16:23
+        db 09Ah                 ; Present, Ring 0, Code, Non-conforming, Readable
+        db 0CFh                 ; Page-granular
+        db 0                    ; Base 24:31
+
+KERNEL_DATA             equ $-gdt
+gdt_kernel_data:
+        dw 0FFFFh               ; Limit 0xFFFF
+        dw 0                    ; Base 0:15
+        db 0                    ; Base 16:23
+        db 092h                 ; Present, Ring 0, Data, Expand-up, Writable
+        db 0CFh                 ; Page-granular
+        db 0                    ; Base 24:32
+
+gdt_interrupts:
+        dw 0FFFFh
+        dw 01000h
+        db 0
+        db 10011110b
+        db 11001111b
+        db 0
+
+gdt_end:                        ; Used to calculate the size of the GDT
+
+gdt_desc:                       ; The GDT descriptor
+        dw gdt_end - gdt - 1    ; Limit (size)
+        dd gdt                  ; Address of the GDT
+
+;------------------------------------------------------------------------------
+bits 32
+init_pm:
+    mov ax, KERNEL_DATA
+    mov ds, ax
+    mov ss, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    
+    mov ebp, 0x90000
+    mov esp, ebp
+
+    mov edx, 0xb8000
+    mov al, 'X'
+    mov ah, 0x2f
+    mov [edx], ax
+
+    add edx, 30
+    mov word [edx], 0x1234
+
+    jmp $
+
+db 'X_END_X'
+
 times 510-($-$$) db 0   ; Pad remainder of boot sector with 0s
 dw 0xAA55               ; boot signature
-
-times 256 dw 0xdada             ; 2nd sector
-times 256 dw 0xface             ; 3rd sector
