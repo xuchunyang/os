d08053f881e52cfefdaa839b984bc32f5f321bcb
Chunyang Xu <xuchunyang56@gmail.com>
2014-10-25 01:07:14 +0800

    Read disk to load kernel

--- Makefile
+++ Makefile
@@ -1,7 +1,18 @@
+CC = i386-elf-gcc
+LD = i386-elf-ld
+
+CFLAGS = -ffreestanding -fno-builtin -nostdlib -nostdinc
+CFLAGS += -g -Wall
+
 all:  floppy.img
 
-floppy.img: boot_sect.bin
-	cat boot_sect.bin /dev/zero | dd bs=512 count=2880 of=floppy.img
+floppy.img: boot_sect.bin kernel.bin
+	cat boot_sect.bin kernel.bin /dev/zero | dd bs=512 count=2880 of=floppy.img
+
+# '0x1000' find correct address of labels
+kernel.bin: kernel.c
+	${CC} ${CFLAGS} -c kernel.c -o kernel.o
+	${LD} -o kernel.bin -Ttext 0x1000 kernel.o --oformat binary
 
 boot_sect.bin: boot_sect.asm
 	nasm -f bin $< -o $@
--- boot_sect.asm
+++ boot_sect.asm
@@ -3,17 +3,20 @@
 bits 16                   
 org 0x7c00                      ; tell NASM to correct address of label
 
+KERNEL_OFFSET equ 0x1000 ; This is the memory offset to which we will load our kernel
+
 start:
+    mov [BOOT_DRIVE], dl        ; Store boot driver in DL
+
     mov bp, 0x8000              ; Set stack in a safe place
     mov sp, bp                  ; at 0x8000
 
     mov si, boot_msg
     call print_str
 
-    ; todo load kernel (from disk)
+    call load_kernel
 
 ;----------Entering Protected Mode----------;
-enter_pm:
     cli
     lgdt [gdt_desc]         ; Load the GDT descriptor
 
@@ -23,6 +26,46 @@ enter_pm:
 
     jmp KERNEL_CODE:init_pm ; Jump to code segment, offset init_pm
 
+    jmp $
+
+
+;---------------------------------------------------------------
+load_kernel:
+    mov  si, LOAD_KERNEL_MSG
+    call print_str
+
+    ; todo load kernel (from disk)
+    mov bx, KERNEL_OFFSET       ; Load 15 sector(2-16) to 0x0000(ES):0x1000(BX)
+    mov dh, 15                  ; from disk
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+
+    ret
+
+;---------------------------------------------------------------
+; load DH sectors to ES:BX from drive DL
+disk_load:
+    push dx                     ; Store DX on stack so later we can recall
+                                ; how many sectors were request to be read
+    mov ah, 0x02                ; BIOS read sector function
+    mov al, dh                  ; Read DH sectors
+    mov ch, 0x00                ; Select cylinder 0
+    mov dh, 0x00                ; Select head 0
+    mov cl, 0x02                ; Start reading from second sector (i.e.
+                                ; after the boot sector)
+    int 0x13                    ; BIOS interrupt (INT 0x13, AH = 2 -- read floppy/hard disk in CHS mode)
+
+    jc disk_error               ; Jump if error (i.e. carry flag set)
+
+    pop dx                      ; Restore DX from the stack
+    cmp dh, al                  ; if AL (sectors read) != DH (sectors expected)
+    jne disk_error              ;    display error message
+    ret
+
+disk_error:
+
+    mov si, DISK_ERROR_MSG
+    call print_str
     jmp $
 
 ;---------------------------------------------------------------
@@ -45,8 +88,9 @@ HEX_OUT  db '0x0000', 0
 hexstr   db '0123456789ABCDEF'
 outstr16 db '0x0000', 0  ;register value string
 reg16    dw    0         ; pass values to printreg16
-DISK_ERROR_MSG db "Disk read error!", 0
+DISK_ERROR_MSG db 'Disk read error!', 0
 BOOT_DRIVE db 0
+LOAD_KERNEL_MSG db 'Loading Kernel...', 0
 
 ;----------Global Descriptor Table----------;
 
@@ -75,14 +119,6 @@ gdt_kernel_data:
         db 0CFh                 ; Page-granular
         db 0                    ; Base 24:32
 
-gdt_interrupts:
-        dw 0FFFFh
-        dw 01000h
-        db 0
-        db 10011110b
-        db 11001111b
-        db 0
-
 gdt_end:                        ; Used to calculate the size of the GDT
 
 gdt_desc:                       ; The GDT descriptor
@@ -99,16 +135,7 @@ init_pm:
     mov fs, ax
     mov gs, ax
     
-    mov ebp, 0x90000
-    mov esp, ebp
-
-    mov edx, 0xb8000
-    mov al, 'X'
-    mov ah, 0x2f
-    mov [edx], ax
-
-    add edx, 30
-    mov word [edx], 0x1234
+    call KERNEL_OFFSET
 
     jmp $
 
