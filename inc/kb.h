5bc58f504c463e7d5cede39a46c5f2aa1cc7f9cc
Chunyang Xu <xuchunyang56@gmail.com>
2014-11-23 21:07:11 +0800

    Clean up, delete everything

--- inc/kb.h
+++
@@ -1,8 +0,0 @@
-#ifndef KB_H
-#define KB_H
-
-#include "common.h"
-
-void init_kb();
-
-#endif
--- loader.asm
+++
@@ -1,7 +0,0 @@
-; Loader.asm
-bits 32
-extern kmain
-global start
-
-start:
-  call kmain ; Call our kernel's kmain() function
